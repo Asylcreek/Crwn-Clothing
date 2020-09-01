7c2f83f1e31a2ec8e9df8830c8cc318265340629
Boro Omokugbo <omokugbobr@gmail.com>
2020-09-01 08:49:42 +0100

    Start adding sagas to project.

--- src/Pages/shopPage/shopPage.component.jsx
+++ src/Pages/shopPage/shopPage.component.jsx
@@ -2,35 +2,36 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import { connect } from 'react-redux';
 
-import { fetchCollectionsStartAsync } from '../../redux/shop/shop.actions';
+import { fetchCollectionsStart } from '../../redux/shop/shop.actions';
 
 import CollectionsOverviewContainer from '../../Components/collection-overview/collection-overview.container';
 import CollectionPageContainer from '../collectionPage/collectionPage.container';
 
-const ShopPage = (props) => {
-  const { fetchCollectionsStartAsync } = props;
+class ShopPage extends React.Component {
+  componentDidMount() {
+    const { fetchCollectionsStart } = this.props;
+    fetchCollectionsStart();
+  }
 
-  React.useEffect(() => fetchCollectionsStartAsync(), [
-    fetchCollectionsStartAsync,
-  ]);
-
-  return (
-    <div className="shop-page">
-      <Route
-        exact
-        path={`${props.match.path}`}
-        component={CollectionsOverviewContainer}
-      />
-      <Route
-        path={`${props.match.path}/:collectionId`}
-        component={CollectionPageContainer}
-      />
-    </div>
-  );
-};
+  render() {
+    return (
+      <div className="shop-page">
+        <Route
+          exact
+          path={`${this.props.match.path}`}
+          component={CollectionsOverviewContainer}
+        />
+        <Route
+          path={`${this.props.match.path}/:collectionId`}
+          component={CollectionPageContainer}
+        />
+      </div>
+    );
+  }
+}
 
 const mapDispatchToProps = (dispatch) => ({
-  fetchCollectionsStartAsync: () => dispatch(fetchCollectionsStartAsync()),
+  fetchCollectionsStart: () => dispatch(fetchCollectionsStart()),
 });
 
 export default connect(null, mapDispatchToProps)(ShopPage);
